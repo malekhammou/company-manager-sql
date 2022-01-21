40dbde846e8423df13a089b995676ba2dedd6c54
malekhammou <malekhammou2017@gmail.com>
2022-01-21 16:14:07 +0100

    Finished general queries

--- GENERAL-QUERIES.sql
+++ GENERAL-QUERIES.sql
@@ -45,3 +45,65 @@ from
 WHERE
     NumServP = 10
     and PosteP IN ('DIRECTEUR', 'SECRETAIRE');
+
+--Q9--
+SELECT
+    NomP
+from
+    Personne
+WHERE
+    NumServP NOT IN (10, 20, 60);
+
+--Q10--
+SELECT
+    NomP,
+    PosteP,
+    SalaireP
+from
+    Personne
+ORDER BY
+    PosteP,
+    SalaireP DESC;
+
+--Q11--
+SELECT
+    COUNT(DISTINCT NumREsProj)
+FROM
+    Projet;
+
+--Q12--
+SELECT
+    MAX(SalaireP)
+FROM
+    Personne
+WHERE
+    PosteP = 'INGENIEUR';
+
+--Q13--
+SELECT
+    AVG(SalaireP)
+from
+    Personne
+WHERE
+    PosteP = 'SECRETAIRE';
+
+--Q14--
+SELECT
+    NomP
+FROM
+    Personne
+WHERE
+    DateEmbP =(
+        SELECT
+            MIN(DateEmbP)
+        FROM
+            Personne
+    );
+
+--Q15--
+SELECT
+    NomP
+from
+    Personne
+WHERE
+    NomP LIKE '_E%M';
