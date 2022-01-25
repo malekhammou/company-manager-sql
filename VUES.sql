3258d2c31fc6a1aabd577dd4a192f00f18291620
malekhammou <malekhammou2017@gmail.com>
2022-01-25 15:22:59 +0100

    started views lab

---
+++ VUES.sql
@@ -0,0 +1,45 @@
+--Q1--
+CREATE VIEW emp_sec AS
+SELECT
+    *
+from
+    Personne
+WHERE
+    PosteP = 'SECRETAIRE';
+
+--Q2--
+insert into
+    emp_sec
+values
+    (
+        8001,
+        'ZAYANI',
+        'INGENIEUR',
+        TO_DATE('11-03-05', 'DD-MM-YYYY'),
+        900,
+        60
+    );
+
+insert into
+    emp_sec
+values
+    (
+        8005,
+        'TLILI',
+        'SECRETAIRE',
+        TO_DATE('03-09-04', 'DD-MM-YYYY'),
+        350,
+        10
+    );
+
+INSERT INTO
+    emp_sec2
+VALUES
+    (
+        8298,
+        'MALEK',
+        'SECRETAIRE',
+        TO_DATE('11-03-05', 'DD-MM-YYYY'),
+        900,
+        60
+    );
